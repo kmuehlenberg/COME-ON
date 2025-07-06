5ff52d08fe95ad493cd09b65c4b524787cd49521
kmuehlenberg <muehlenbergz55@gmail.com>
2025-07-05 23:14:46 -0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', padding: '2rem' }}>
-      <h1>Welcome to Valestra</h1>
-      <p>Your journey through the Vault begins here.</p>
-    </div>
-  )
-}
